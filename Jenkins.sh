0136c0692a61b3a33838470f44b5f15ea69c8de4
Ali Shehzad <princealishehzad943@gmail.com>
2025-06-30 16:38:24 +0500

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -19,16 +19,48 @@ sudo alternatives --config java
 # (Choose /usr/lib/jvm/java-17-amazon-corretto.x86_64/bin/java)
 
 # STEP 7: Start Jenkins
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
-
+sudo systemctl start jenkins.service
+sudo systemctl enable jenkins.service
 # STEP 8: Check versions
 java -version      # should show java 17
 mvn -version       # should also show java 17 being used
 
-
 #then copy Public ip :8080 and paste on browser 
 #Start jenkins ,path copy ,paste on SSH and enter code
+============================================================================
+
+#Jenkins Prameter:
+String Parameter
+    Takes a single line of text input from the user.
+Choice Parameter**
+  Shows a dropdown menu with predefined choices.
+Multi-line String Parameter**
+        Allows user to input multiple lines of text.
+Boolean Parameter
+         A checkbox to get true/false (yes/no) input.
+File Parameter
+       Allows users to upload a file when triggering a build.
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
