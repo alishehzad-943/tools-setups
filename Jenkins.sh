49c9c9c11083822095e273ee0f315712535a8008
Ali Shehzad <princealishehzad943@gmail.com>
2025-06-30 12:01:13 +0500

    Create Jenkins.sh

---
+++ Jenkins.sh
@@ -0,0 +1,45 @@
+# STEP 1: System Update
+sudo yum update -y
+
+# STEP 2: Install Java 17 (Amazon Corretto)
+sudo yum install java-17-amazon-corretto -y
+
+# STEP 3: Install Git & Maven
+sudo yum install git maven -y
+
+# STEP 4: Add Jenkins repo & key
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+# STEP 5: Install Jenkins
+sudo yum install jenkins -y
+
+# STEP 6: Ensure Java 17 is default
+sudo alternatives --config java
+# (Choose /usr/lib/jvm/java-17-amazon-corretto.x86_64/bin/java)
+
+# STEP 7: Start Jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+# STEP 8: Check versions
+java -version      # should show java 17
+mvn -version       # should also show java 17 being used
+
+
+#then copy Public ip :8080 and paste on browser 
+#Start jenkins ,path copy ,paste on SSH and enter code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
